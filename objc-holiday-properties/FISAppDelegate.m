6f087efa1a84b000572041d3cf40c9956f1be106
bmanjuu <bettyofung@gmail.com>
2016-06-08 17:25:17 -0400

    holiday properties lab complete

--- objc-holiday-properties/FISAppDelegate.m
+++ objc-holiday-properties/FISAppDelegate.m
@@ -11,37 +11,58 @@
 
 - (NSArray *)holidaysInSeason:(NSString *)season {
     
-    return nil;
+    return self.database[season];
 }
 
 - (NSArray *)suppliesInHoliday:(NSString *)holiday
                       inSeason:(NSString *)season {
     
-    return nil;
+    NSMutableArray *supplies = [self.database[season][holiday] mutableCopy];
+    
+    return supplies;
 }
 
 - (BOOL)holiday:(NSString* )holiday
      isInSeason:(NSString *)season {
     
-    return nil;
+    NSArray *holidaysInSeason = [self holidaysInSeason:season];
+    
+    for(NSString *holidays in holidaysInSeason){
+        if([holidays isEqualToString:holiday]){
+            return YES;
+        }
+    }
+    
+    return NO;
 }
 
 - (BOOL)supply:(NSString *)supply
    isInHoliday:(NSString *)holiday
       inSeason:(NSString *)season {
     
-    return nil;
+    NSArray *suppliesAvailable = [self suppliesInHoliday:holiday inSeason:season];
+    
+    for(NSString *supplies in suppliesAvailable){
+        if([supplies isEqualToString:supply]){
+            return YES;
+        }
+    }
+    
+    return NO;
 }
 
 - (void)addHoliday:(NSString *)holiday
           toSeason:(NSString *)season {
     
+    self.database[season][holiday] = [[NSMutableArray alloc] init];
     // no return
 }
 
 - (void)addSupply:(NSString *)supply
         toHoliday:(NSString *)holiday
          inSeason:(NSString *)season {
+    
+    [self.database[season][holiday] addObject:supply]; 
     
     // no return
 }
